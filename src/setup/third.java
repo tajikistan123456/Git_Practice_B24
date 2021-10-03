eb4c13eeb3e5c63908a65c761433e11e89873924
cybertek-24 <88249501+cybertek-24@users.noreply.github.com>
2021-10-03 12:45:35 -0400

    added third file

---
+++ src/setup/third.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class third {
+
+
+    public void n(){
+        System.out.println("third class");
+    }
+    public void n(int m){
+        System.out.println("have fun");
+    }
+
+}
