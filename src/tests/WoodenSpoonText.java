79a4ae35faa2ec6ef7ba890f014474db81aaa527
cybertek-24 <88249501+cybertek-24@users.noreply.github.com>
2021-10-03 16:29:58 -0400

    added ws

---
+++ src/tests/WoodenSpoonText.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class WoodenSpoonText {
+    public static void main(String[] args) {
+
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Wooden Spoon");
+        }
+
+    }
+}
